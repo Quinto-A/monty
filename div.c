4a09df6146bcf312eebf87fc0054499cf805fd15
Quinto <116285888+Quinto-A@users.noreply.github.com>
2024-01-18 23:15:35 +0300

    Update div.c

--- div.c
+++ div.c
@@ -10,7 +10,7 @@
 void f_div(stack_t **head, unsigned int counter)
 {
 	stack_t *h;
-	int len = 0, aux;
+	int len = 0, ptr;
 
 	h = *head;
 	while (h)
@@ -35,8 +35,8 @@ void f_div(stack_t **head, unsigned int counter)
 		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	aux = h->next->n / h->n;
-	h->next->n = aux;
+	ptr = h->next->n / h->n;
+	h->next->n = ptr;
 	*head = h->next;
 	free(h);
 }
